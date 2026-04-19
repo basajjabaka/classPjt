51e607aecfa0226ced85baf79bb7d8f6f8439d1a
basajjabaka katende <19basajjabaka@gmail.com>
2026-04-19 22:24:30 +0300

    nzuuno wendi

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+    return (
+        <div>
+          Welcome to my first dashboard!
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ src/pages/subjects/list.tsx
@@ -0,0 +1,9 @@
+const SubjectsList = () => {
+    return (
+        <div>
+            Welcome to my first list!
+        </div>
+    )
+}
+
+export default SubjectsList;
